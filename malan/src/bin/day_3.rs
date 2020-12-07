723da0aa2fbae0d2283fb5922948e8cd8cd7f779
= <=>
2020-12-07 21:02:43 +0200

    feat: cycle iterator

--- malan/src/bin/day_3.rs
+++ malan/src/bin/day_3.rs
@@ -7,14 +7,15 @@ enum Terrain {
     TREE = b'#'
 }
 
-// Specify a Direction as simply enum
+// Specify a Direction as simple enum
+#[derive(Debug)]
 enum Direction {
     UP,
     DOWN,
     RIGHT,
     LEFT,
 }
-// A Toboggan holds the logic for how it moves
+// A Vehicle holds the logic for how it moves
 struct Vehicle {
     move_seqeunce: Vec<Direction>,
 }
@@ -25,27 +26,19 @@ struct Map {
 }
 
 trait Traverse {
-    fn traverse(&self, direction: Direction) -> Terrain;
-}
-
-
-// A Driver has-a vehicle and has-a map.
-// She drives the vehicle, and check the map
-struct Driver {
-    vehicle: Vehicle,
-    map: Map
-}
-
-impl Driver {
-    fn new(vehicle: Vehicle, map: Map) -> Driver {
-        Driver {vehicle, map}
-    }
+    fn traverse(&self, direction: &Direction) -> &Terrain;
 }
 
 fn main() {
     println!("Day 3 Puzzle");
     let input_string = include_str!("../inputs/03.txt");
-    let cheap_toboggan = Vehicle { move_seqeunce: vec![ Direction::RIGHT, Direction::RIGHT, Direction::RIGHT, Direction::DOWN ] };
-    let map = Map {file: 0, rank: 0 };
-    let driver = Driver::new(cheap_toboggan, map);
+    // let cheap_toboggan = Vehicle { move_seqeunce: vec![ Direction::RIGHT, Direction::RIGHT, Direction::RIGHT, Direction::DOWN ] };
+    // let map = Map {file: 0, rank: 0 };
+    // let driver = Driver::new(cheap_toboggan, map);
+    let cheap_toboggan = vec![ Direction::RIGHT, Direction::RIGHT, Direction::RIGHT, Direction::DOWN ];
+    let mut it = cheap_toboggan.iter().cycle();
+
+    loop {
+        let a = it.next().expect("OOPS");
+    }
 }
