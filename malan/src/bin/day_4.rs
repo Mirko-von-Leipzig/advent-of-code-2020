0a7c37ca2603bfd527b2c544a956e24e866fa49f
= <=>
2020-12-13 19:10:52 +0200

    feat: adding day 4 framework

---
+++ malan/src/bin/day_4.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("Day 4");
+    let _input_string = include_str!("../inputs/04.txt");
+    puzzle_1(_input_string);
+    puzzle_2(_input_string);
+}
+
+fn puzzle_1(input: &str) {
+    println!("Running Puzzle 1")
+}
+
+fn puzzle_2(input: &str) {
+    
+    println!("Running Puzzle 2")
+}
+
+#[cfg(test)]
+mod tests {
+    
+    #[test]
+    fn test_puzzle_1() {
+        assert!(true);
+    }
+
+    #[test]
+    fn test_puzzle_2() {
+        assert!(true);
+    }
+}
